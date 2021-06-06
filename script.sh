131a8e45b6b1611d6dda4aa320fe45282749162c
241207BCLN <83624529+241207BCLN@users.noreply.github.com>
2021-06-06 13:29:30 +0800

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# This is a comment!
+# echo "Aim high, STI!"
+# echo "This is a comment!"
+# echo "This is a comment"
+
+#!/bin/sh
+word1=Hello
+word2=How
+word3=you
+word4=are
+echo "$word2 $word4 $word2? $word1?!?"
+
+#!/bin/sh
+Name1=John
+echo "Who are you?"
+Name2=Michael
+echo "Nice to meet you $Name1"
+
+#!/bin/sh
+aa=4
+ab=5
+ac=6
+let "aa=aa+ac"
+echo "The first operation is equal to $aa"
+let "ac=ac-ab"
+echo "The second operation is equal to $ac"
